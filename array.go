00f52e0ef9ad2db857c736d5209d2584989d5b6b
zfygood <zfy427@gmail.com>
2022-04-06 00:59:17 +0800

    alg

--- array.go
+++ array.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sync"
 )
 
@@ -325,6 +326,306 @@ func getNumOdd(arr []int) {
 		}
 	}
 
+}
+
+func findSmallK(arr []int, l, h, k int) int {
+	i := l
+	j := h
+	tmp := arr[i]
+	for i < j {
+		for i < j && arr[j] >= tmp {
+			j--
+		}
+		if i < j {
+			arr[i] = arr[j]
+			i++
+		}
+		for i < j && arr[i] <= tmp {
+			i++
+		}
+		if i < j {
+			arr[j] = arr[i]
+			j--
+		}
+	}
+	arr[i] = tmp
+	index := i - l
+	if index == k-1 {
+		return tmp
+	} else if index > k-1 {
+		return findSmallK(arr, l, i-1, k)
+	} else {
+		return findSmallK(arr, i+1, h, k-(i-l)-1)
+	}
+
+}
+
+func findSmallKK(arr []int, k, j int) int {
+	j++
+	tmp := arr[0]
+	//fmt.Println(arr)
+	for i := 1; i < len(arr); i++ {
+		if tmp > arr[i] {
+			arr[0] = arr[i]
+			arr[i] = tmp
+			tmp = arr[0]
+		}
+	}
+	if j == k {
+		return arr[0]
+	}
+	return findSmallKK(arr[1:], k, j)
+}
+
+func findTop(arr []int) (r1, r2, r3 int) {
+	if arr == nil || len(arr) < 3 {
+		return
+	}
+
+	r1 = math.MinInt64
+	r2 = math.MinInt64
+	r3 = math.MinInt64
+
+	for _, v := range arr {
+		if v > r1 {
+			r3 = r2
+			r2 = r1
+			r1 = v
+			fmt.Println(r1, r2, r3)
+		} else if v > r2 && v != r1 {
+			r3 = r2
+			r2 = v
+			fmt.Println(r1, r2, r3)
+		} else if v > r3 && v != r2 {
+			r3 = v
+			fmt.Println(r1, r2, r3)
+		}
+	}
+	return
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minSan(a, b, c int) int {
+	var m int
+	if a < b {
+		m = a
+	} else {
+		m = b
+	}
+	if m > c {
+		m = c
+	}
+	return m
+}
+func max(a, b, c int) (m int) {
+	if a < b {
+		m = b
+	} else {
+		m = a
+	}
+	if m < c {
+		m = c
+	}
+	return
+}
+
+func abs(m int) int {
+	if m < 0 {
+		return -m
+	}
+	return m
+}
+
+func minDistance(arr []int, num1, num2 int) int {
+	if arr == nil || len(arr) <= 0 {
+		return math.MaxInt64
+	}
+
+	lastPos1 := -1
+	lastPos2 := -1
+	minDis := math.MaxInt64
+	for i, v := range arr {
+		if v == num1 {
+			lastPos1 = i
+			if lastPos2 >= 0 {
+				minDis = min(minDis, lastPos1-lastPos2)
+			}
+		}
+
+		if v == num2 {
+			lastPos2 = i
+			if lastPos1 >= 0 {
+				minDis = min(minDis, lastPos2-lastPos1)
+			}
+		}
+	}
+	return minDis
+}
+
+func minDistanceSan(a, b, c []int) int {
+	aLen := len(a)
+	bLen := len(b)
+	cLen := len(c)
+	minDist := math.MaxInt64
+	i, j, k := 0, 0, 0
+	for {
+		curDist := max(abs(a[i]-b[j]), abs(b[j]-c[k]), abs(a[i]-c[k]))
+		if curDist < minDist {
+			minDist = curDist
+		}
+		m := minSan(a[i], b[j], c[k])
+		if m == a[i] {
+			i++
+			if i >= aLen {
+				break
+			}
+		} else if m == b[j] {
+			j++
+			if j >= bLen {
+				break
+			}
+		} else {
+			k++
+			if k >= cLen {
+				break
+			}
+		}
+	}
+	return minDist
+}
+
+func findMinMath(arr []int) int {
+	if arr == nil || len(arr) == 0 {
+		return -1
+	}
+	begin := 0
+	mid := 0
+	end := len(arr) - 1
+	var absMin int
+	if arr[0] > 0 {
+		return arr[0]
+	}
+	if arr[end] <= 0 {
+		return arr[end]
+	}
+
+	for {
+		mid = begin + ((end - begin) >> 1)
+		if arr[mid] == 0 {
+			return 0
+		} else if arr[mid] > 0 {
+			if arr[mid-1] > 0 {
+				end = mid - 1
+			} else if arr[mid-1] == 0 {
+				return 0
+			} else {
+				break
+			}
+		} else {
+			if arr[mid+1] < 0 {
+				begin = mid + 1
+			} else if arr[mid+1] == 0 {
+				return 0
+			} else {
+				break
+			}
+		}
+	}
+
+	if arr[mid] > 0 {
+		if arr[mid] < abs(arr[mid-1]) {
+			absMin = arr[mid]
+		} else {
+			absMin = arr[mid-1]
+		}
+	} else {
+		if abs(arr[mid]) < arr[mid+1] {
+			absMin = arr[mid]
+		} else {
+			absMin = arr[mid+1]
+		}
+	}
+	return absMin
+}
+
+func findMin(arr []int) int {
+	if arr == nil || len(arr) == 0 {
+		return -1
+	}
+
+	absMin := math.MaxInt64
+	for _, v := range arr {
+		if abs(v) < abs(absMin) {
+			absMin = v
+		}
+	}
+	return absMin
+}
+
+func maxSubArraySum(arr []int) int {
+	if arr == nil || len(arr) < 1 {
+		return -1
+	}
+	l := len(arr)
+	maxSum := math.MinInt64
+	for i := 0; i < l; i++ {
+		sum := 0
+		for j := i; j < l; j++ {
+			sum += arr[j]
+			if sum > maxSum {
+				maxSum = sum
+			}
+			fmt.Println(arr[j], sum, maxSum)
+
+		}
+	}
+	return maxSum
+}
+func MaxRe(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+
+func maxSubArraySumD(arr []int) int {
+	nAll := arr[0]
+	nEnd := arr[0]
+	for _, v := range arr {
+		fmt.Println(nEnd+v, v)
+		nEnd = MaxRe(nEnd+v, v)
+		fmt.Println(nEnd)
+		nAll = MaxRe(nEnd, nAll)
+		fmt.Println(nAll, nEnd)
+	}
+	return nAll
+}
+
+func maxSubArrayEx(arr []int) (maxSum, begin, end int) {
+	maxSum = math.MinInt64
+	nSum := 0
+	nStart := 0
+
+	for i, v := range arr {
+		if nSum < 0 {
+			nSum = v
+			nStart = i
+		} else {
+			nSum += v
+		}
+
+		if nSum > maxSum {
+			maxSum = nSum
+			begin = nStart
+			end = i
+		}
+	}
+	return
 }
 
 func main() {
